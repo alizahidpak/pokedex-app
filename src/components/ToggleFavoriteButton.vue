17badd5f1e93ef356922b94bf222a9bd32798b41
alizahidpak <alizahidpak@gmail.com>
2023-12-25 07:25:58 +0300

    Refactor(ToggleFavoriteButton): Moved ToggleButton to ToggleFavoriteButton

---
+++ src/components/ToggleFavoriteButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <button @click="toggleFavorite">
+        <img
+            :class="{ 'is-favorite': pokemon.isFavorite }"
+            alt="Favorite"
+            src="@/assets/img/star.png"
+        />
+    </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { pokemonStore } from "@/store/pokemon";
+import type { PokemonType } from "@/types";
+
+@Component
+export default class ToggleFavoriteButton extends Vue {
+    @Prop({ default: () => ({}), type: Object }) readonly pokemon!: PokemonType;
+
+    toggleFavorite() {
+        pokemonStore.TOGGLE_FAVORITE(this.pokemon);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 0;
+    transition: 0.5s;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+img {
+    transition: 0.5s;
+    filter: grayscale(100%);
+}
+
+.is-favorite {
+    filter: grayscale(0%);
+}
+
+img {
+    width: 3.2rem;
+    height: 3.2rem;
+}
+</style>
