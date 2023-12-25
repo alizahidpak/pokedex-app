bc46b5ada080cdb3d92dd68f522a3dd464f7d201
alizahidpak <alizahidpak@gmail.com>
2023-12-25 07:04:47 +0300

    Style: Removed animate.css, added custom animation.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
-  <div id="app">
-    <MainHeader />
-    <TransitionFade>
-      <router-view />
-    </TransitionFade>
-  </div>
+    <div id="app">
+        <MainHeader />
+        <TransitionFade>
+            <router-view />
+        </TransitionFade>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { MainHeader, TransitionFade } from "@/components";
-import "animate.css";
 
 @Component({
-  components: {
-    MainHeader,
-    TransitionFade,
-  },
+    components: {
+        MainHeader,
+        TransitionFade,
+    },
 })
 export default class App extends Vue {}
 </script>
--- src/components/LoaderPokeball.vue
+++ src/components/LoaderPokeball.vue
@@ -1,16 +1,31 @@
 <template>
-  <div
-    class="pokeball animate__animated animate__bounce animate__infinite"
-  ></div>
+    <div class="pokeball"></div>
 </template>
 
 <style lang="scss" scoped>
+@keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+        transform: translateY(0);
+    }
+    40% {
+        transform: translateY(-30px);
+    }
+    60% {
+        transform: translateY(-15px);
+    }
+}
+
 .pokeball {
-  width: 10rem;
-  height: 10rem;
-  background-image: url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png");
-  background-size: contain;
-  background-position: center;
-  margin: 0 auto;
+    width: 10rem;
+    height: 10rem;
+    background-image: url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png");
+    background-size: contain;
+    background-position: center;
+    margin: 0 auto;
+    animation: bounce 2s infinite;
 }
 </style>
